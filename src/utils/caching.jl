4459626e8f6aa56d9093bc70c9fbb28fa86e877a
krynju <gulinski.krystian@gmail.com>
2023-04-10 18:10:38 +0200

    improve internal interface

--- src/utils/caching.jl
+++ src/utils/caching.jl
@@ -1,18 +1,16 @@
-function enable_disk_caching!(ram_percentage_limit::Int=30)
-    !(0 < ram_percentage_limit <= 100 ) && error("Ram limit values must be in (1, 100> range")
-    processes = procs()
+function enable_disk_caching!(
+    ram_percentage_limit::Int=30, processes::Vector{Int}=procs(), disk_limit_gb::Int=16
+)
+    !(0 < ram_percentage_limit <= 100) &&
+        error("Ram limit values must be in (1, 100> range")
 
-    process_info = [ id =>
-        remotecall(id) do
-            return (;
-                total_memory=Sys.total_physical_memory(),
-                hostname=gethostname(),
-            )
-        end
-        for id in processes
+    process_info = [
+        id => remotecall(id) do
+            return (; total_memory=Sys.total_physical_memory(), hostname=gethostname())
+        end for id in processes
     ]
 
-    machines = Dict()
+    machines = Dict{Int,Any}()
     for (id, info) in process_info
         key = fetch(info)
         machines[key] = push!(get(machines, key, Int[]), id)
@@ -25,24 +23,47 @@ function enable_disk_caching!(ram_percentage_limit::Int=30)
         end
     end
 
-    r = [
+    return enable_disk_caching!(mem_limits, processes, disk_limit_gb)
+end
+
+function enable_disk_caching!(
+    mem_limits::Dict{Int,Int}, processes::Vector{Int}=procs(), disk_limit_gb::Int=16
+)
+    results = [
         remotecall(id) do
-            Main.eval(:(using Dagger))
+            !isdefined(Main, :Dagger) && Main.eval(:(using Dagger))
             Dagger.MemPool.setup_global_device!(
                 Dagger.MemPool.DiskCacheConfig(;
-                    toggle=true,
-                    membound=mem_limits[id]
-                )
+                    toggle=true, membound=mem_limits[id], diskbound=disk_limit_gb * 2^30
+                ),
             )
-            true
-        end
-        for id in processes
+            nothing
+        end for id in processes
     ]
-
-    return try
-        all(fetch.(r))
-    catch _
-        @error("Error when setting up disk caching on all workers")
-        false
+    any_error = false
+    for (i, id) in enumerate(processes)
+        r = fetch(results[i])
+        any_error |= r !== nothing
+        if r !== nothing
+            @error("Error setting up disk caching on process id = $id", ex = r)
+        end
     end
+
+    return if any_error
+        @error("Disk cache setup failed")
+        false
+    else
+        @info("Disk cache setup successful")
+        true
+    end
+end
+
+function inspect_global_devices(processes::Vector{Int}=procs())
+    results = [
+        remotecall(id) do
+            !isdefined(Main, :Dagger) && Main.eval(:(using Dagger))
+            id => Dagger.MemPool.GLOBAL_DEVICE[]
+        end for id in processes
+    ]
+    return fetch.(results)
 end
